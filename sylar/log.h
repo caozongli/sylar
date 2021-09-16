bc51a45a6a85c6f1e4161a1822744133bb9fb1c6
caozongli <932478335@qq.com>
2021-09-16 20:42:13 +0800

    log accomplish

--- sylar/log.h
+++ sylar/log.h
@@ -4,16 +4,36 @@
 #include <string>
 #include <stdint.h>
 #include <memory>
+#include <list>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <time.h>
+#include <string.h>
 
 namespace sylar
 {
+
+class Logger;
 
 //日志事件
 class LogEvent
 {
 public:
 	typedef std::shared_ptr<LogEvent> ptr;
-	LogEvent();
+	LogEvent(const char* file, int32_t m_line, uint32_t elapse, uint32_t thread_id, uint32_t fiber_id, uint64_t time);
+
+	const char* getFile() const {return m_file;}
+	int32_t getLine() const {return m_line; }
+	uint32_t getElapse() const { return m_elapse; } 
+	uint32_t getThreadId() const { return m_threadId; }
+	uint32_t getFiberId() const { return m_fiberId; }
+	uint64_t getTime() const { return m_time; }
+	std::string getContent() const { return m_ss.str(); }
+	std::stringstream& getSS() {return m_ss;}
+
 private:
 	const char* m_file = nullptr; 	//文件名
 	int32_t m_line = 0;				//行号
@@ -21,7 +41,7 @@ private:
 	int32_t m_threadId = 0;			//线程id
 	uint32_t m_fiberId = 0; 		//协程号
 	uint64_t m_time = 0;			//时间戳
-	std::string m_content;			//内容
+	std::stringstream m_ss;			//内容
 };
 
 //日志级别
@@ -34,6 +54,7 @@ public:
 		ERROR = 4,
 		FATAL = 5
 	};
+	static const char* ToString(LogLevel::Level level);
 };
 
 //日志格式器
@@ -41,8 +62,25 @@ class LogFormatter
 {
 public:
 	typedef std::shared_ptr<LogFormatter> ptr;
-	std::string format(LogEvent::ptr event);
+	LogFormatter(const std::string& pattern);
+
+	// %t	thread_id %m%n
+	std::string format(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event);
+public:
+	class FormatItem
+	{
+	public:
+		typedef std::shared_ptr<FormatItem> ptr;
+		virtual ~FormatItem(){};
+		virtual void format(std::ostream& os, std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) = 0;
+	};
+
+	void init();
+
 private:
+	std::string m_pattern;
+	bool m_error = false;
+	std::vector<FormatItem::ptr> m_items;
 };
 
 //日志输出地
@@ -52,40 +90,70 @@ public:
 	typedef std::shared_ptr<LogAppender> ptr;
 	virtual ~LogAppender(){};
 
-	void log(LogLevel::Level level, LogEvent::ptr event);
-private:
-	LogLevel::Level m_level;
+	virtual void log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) = 0;
 
+	void setFormatter(LogFormatter::ptr val){m_formatter = val; }
+	LogFormatter::ptr getFormatter() const { return m_formatter; }
+protected:
+	LogLevel::Level m_level;
+	LogFormatter::ptr m_formatter;
 
 };
 
 
 
 //日志器
-class Logger
+class Logger: public std::enable_shared_from_this<Logger>
 {
 public:
 	typedef std::shared_ptr<Logger> ptr;
-		Logger(const std::string& name = "root");
 
+	Logger(const std::string& name = "root");
 	void log(LogLevel::Level level, const LogEvent::ptr& event);
+
+	void debug(LogEvent::ptr event);
+	void info(LogEvent::ptr event);
+	void warn(LogEvent::ptr event);
+	void error(LogEvent::ptr event);
+	void fatal(LogEvent::ptr event);
+
+	void addAppender(LogAppender::ptr appender);
+	void delAppender(LogAppender::ptr appender);
+	LogLevel::Level getLevel() const { return m_level; }
+	void setLevel(LogLevel::Level val) { m_level = val; }
+
+	const std::string& getName() const { return m_name; }
+
 private:
-	std::string m_name;
-	LogLevel::Level m_level;
-	LogAppender::ptr;
+	std::string m_name;			//日志名称
+	LogLevel::Level m_level;	//日志级别
+	std::list<LogAppender::ptr> m_appenders;	//Apenbder集合
+	LogFormatter::ptr m_formatter;
 
 };
 
 //输出到控制台的Appender
 class StdoutLogAppender:public LogAppender
 {
+public:
+	typedef std::shared_ptr<StdoutLogAppender> ptr;
+	void log(std::shared_ptr<Logger> logger, LogLevel::Level level , LogEvent::ptr event) override;
 
 };
 
 //输出到文件的Appender
 class FileLogAppender: public LogAppender
 {
+public:
+	typedef std::shared_ptr<FileLogAppender> ptr;
+	FileLogAppender(const std::string& filename);
+	void log(std::shared_ptr<Logger> logger, LogLevel::Level level, LogEvent::ptr event) override;
 
+	//重新打开文件 文件打开成功返回true
+	bool reopen();
+private:
+	std::string m_filename;
+	std::ofstream m_filestream;
 };
 
 }
